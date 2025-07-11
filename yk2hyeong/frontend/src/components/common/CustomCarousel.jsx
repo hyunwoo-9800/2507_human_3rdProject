631f4b5e0975044b18bfa865a0a1720ecc3a1bc7
seohay-1ae <mokash1ae@gmail.com>
2025-07-11 15:55:12 +0900

    [update] 배너 컴포넌트 수정

--- yk2hyeong/frontend/src/components/common/CustomCarousel.jsx
+++ yk2hyeong/frontend/src/components/common/CustomCarousel.jsx
@@ -1,43 +1,80 @@
 import React from 'react';
 import { Carousel } from 'antd';
-
-const contentStyle = {
-    height: '200px',
-    background: '#364d79',
-    color: '#fff',
-    userSelect: 'none',
-    cursor: 'pointer',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-};
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
 const CustomCarousel = ({
-                            items = ['첫 번째', '두 번째', '세 번째'],
-                            autoplaySpeed = 3000,
+                            items = [],
+                            height = '250px',
                             autoplay = true,
-                            arrows = true,
-                            dotPosition = 'bottom',
+                            autoplaySpeed = 3000,
                             pauseOnHover = true,
                             onClick,
-                        }) => (
-    <Carousel
-        autoplay={autoplay}
-        autoplaySpeed={autoplaySpeed}
-        arrows={arrows}
-        dotPosition={dotPosition}
-        pauseOnHover={pauseOnHover}  // 여기 pauseOnHover=true 설정
-    >
-        {items.map((item, index) => (
-            <div
-                key={index}
-                onClick={() => onClick && onClick(index, item)}
-                style={contentStyle}
-            >
-                <h3 style={{ margin: 0 }}>{item}</h3>
-            </div>
-        ))}
-    </Carousel>
-);
+                        }) => {
+    // 화살표 스타일
+    const arrowStyle = {
+        fontSize: '24px',
+        color: '#fff',
+        backgroundColor: 'rgba(0,0,0,0.4)',
+        borderRadius: '50%',
+        padding: '8px',
+        position: 'absolute',
+        top: '50%',
+        transform: 'translateY(-50%)',
+        zIndex: 2,
+        cursor: 'pointer',
+    };
+
+    const containerStyle = {
+        position: 'relative',
+        width: '100%',
+        overflow: 'hidden',
+    };
+
+    return (
+        <div style={containerStyle}>
+            <Carousel
+                autoplay={autoplay}
+                autoplaySpeed={autoplaySpeed}
+                pauseOnHover={pauseOnHover}
+                dots
+                dotPosition="bottom"
+                arrows
+               >
+                {items.map((item, index) => (
+                    <div
+                        key={index}
+                        onClick={() => onClick && onClick(index, item)}
+                        style={{ width: '100%' }}
+                    >
+                        <div
+                            style={{
+                                width: '100%',
+                                height: height,
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            {typeof item === 'string' ? (
+                                <img
+                                    src={item}
+                                    alt={`배너${index + 1}`}
+                                    style={{
+                                        width: '100%',
+                                        height: '100%',
+                                        objectFit: 'cover',
+                                        display: 'block',
+                                    }}
+                                />
+                            ) : (
+                                item
+                            )}
+                        </div>
+                    </div>
+                ))}
+            </Carousel>
+        </div>
+    );
+};
 
 export default CustomCarousel;
--- yk2hyeong/frontend/src/pages/index/Home.jsx
+++ yk2hyeong/frontend/src/pages/index/Home.jsx
@@ -4,11 +4,31 @@ import Banner from "../../components/index/Banner";
 import MarketTrend from "../../components/index/MarketTrend";
 import PopularItem from "../../components/index/PopularItem";
 import NoticeBoard from "../../components/index/NoticeBoard";
+import CustomCarousel from "../../components/common/CustomCarousel";
 
 export default function Home() {
+
+    const bannerImages = [
+        <img src="/static/images/banner.png" alt="배너1" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />,
+        <img src="/static/images/banner2.jpg" alt="배너2" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />,
+        <img src="/static/images/banner3.jpg" alt="배너3" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />,
+        <img src="/static/images/banner4.jpg" alt="배너4" style={{ width: '100%', height: '200px', objectFit: 'cover' }} />,
+    ];
+
+
   return (
       <div>
-        <Banner></Banner>
+          <CustomCarousel
+              items={[
+                  '/static/images/banner_tomato.png',
+                  '/static/images/banner_potato.png',
+                  '/static/images/banner_weed.png',
+                  '/static/images/banner_poppy.png',
+                  '/static/images/banner_opium.png',
+              ]}
+              height="300px"
+          />
+        {/*<Banner></Banner>*/}
         <div className="main-content">
           <div className="left-content">
             <MarketTrend/>
