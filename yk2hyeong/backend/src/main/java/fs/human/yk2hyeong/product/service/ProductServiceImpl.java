bac5c13ace06cf757ddb4d4cc8626eabe191d87f
seohay-1ae <mokash1ae@gmail.com>
2025-07-14 11:09:26 +0900

    [add] 상품관련 백엔드 생성

--- yk2hyeong/backend/src/main/java/fs/human/yk2hyeong/product/service/ProductServiceImpl.java
+++ yk2hyeong/backend/src/main/java/fs/human/yk2hyeong/product/service/ProductServiceImpl.java
@@ -1,8 +1,24 @@
 package fs.human.yk2hyeong.product.service;
 
+import fs.human.yk2hyeong.product.dao.ProductDAO;
+import fs.human.yk2hyeong.product.vo.ProductVO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 // 상품관련 서비스 구현
 @Service
 public class ProductServiceImpl implements ProductService {
+    private final ProductDAO productDAO;
+
+    @Autowired
+    public ProductServiceImpl(ProductDAO productDAO) {
+        this.productDAO = productDAO;
+    }
+
+    @Override
+    public List<ProductVO> getAllProducts() {
+        return productDAO.getAllProducts();
+    }
 }
