ae2a8b8a38704347d0c24644010083b742604cf6
seohay-1ae <mokash1ae@gmail.com>
2025-07-25 18:37:37 +0900

    [update] 시세추이 레이아웃

--- yk2hyeong/frontend/src/pages/chart/PriceChartPage.jsx
+++ yk2hyeong/frontend/src/pages/chart/PriceChartPage.jsx
@@ -72,101 +72,99 @@ const PriceChartPage = () => {
   }, [form.midCodeValue])
 
   return (
-    <div style={{ margin: '20px 0 0 0' }}>
-      {/* 선택 박스 */}
-      <div className="input-group">
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            marginBottom: '20px',
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-              marginRight: '20px',
-            }}
-          >
-            <label
-              htmlFor="midCodeValue"
-              className="input-label"
-              style={{ marginRight: '10px', fontSize: '18px' }}
+      <div style={{margin:'40px 0'}}>
+        <h2>시세 추이</h2>
+        <div style={{ margin: '20px 0 0 0' }}>
+          {/* 선택 박스 */}
+          <div className="input-group">
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginBottom: '20px',
+              }}
             >
-              부류
-            </label>
-            <CustomSelect
-              placeholder="부류를 선택하세요"
-              onChange={handleMidCodeSelect}
-              options={midItems}
-              style={{ width: '200px' }}
-            />
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  marginRight: '20px',
+                }}
+              >
+                <label
+                  htmlFor="midCodeValue"
+                  className="input-label"
+                  style={{ marginRight: '10px', fontSize: '18px' }}
+                >
+                  부류
+                </label>
+                <CustomSelect
+                  placeholder="부류를 선택하세요"
+                  onChange={handleMidCodeSelect}
+                  options={midItems}
+                  style={{ width: '200px' }}
+                />
+              </div>
+              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <label
+                  htmlFor="lowCodeValue"
+                  className="input-label"
+                  style={{ marginRight: '10px', fontSize: '18px' }}
+                >
+                  품목
+                </label>
+                <CustomSelect
+                  placeholder="품목을 선택하세요"
+                  onChange={handlelowCodeSelect}
+                  options={lowItems}
+                  style={{ width: '200px' }}
+                />
+              </div>
+            </div>
+
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom:'10px' }}>
+              <label
+                htmlFor="selectedPeriod"
+                className="input-label"
+                style={{ marginRight: '10px', fontSize: '18px' }}
+              >
+                기간
+              </label>
+              <CustomRadio
+                value={form.timeFrame}
+                onChange={handlePeriodChange}
+                options={[
+                  { value: 'week', label: '주간' },
+                  { value: 'month', label: '월간' },
+                  { value: 'year', label: '연간' },
+                ]}
+              />
+            </div>
+            <ReportDownloadButton form={form}></ReportDownloadButton>
           </div>
-          <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-            <label
-              htmlFor="lowCodeValue"
-              className="input-label"
-              style={{ marginRight: '10px', fontSize: '18px' }}
-            >
-              품목
-            </label>
-            <CustomSelect
-              placeholder="품목을 선택하세요"
-              onChange={handlelowCodeSelect}
-              options={lowItems}
-              style={{ width: '200px' }}
-            />
+
+          <div className="viewChart">
+            <h2 style={{ marginTop: '60px',fontSize:'22px' }}>시세 추이 그래프</h2>
+
+            {form.lowCodeValue === '' ? (
+              <p>품목을 선택하면 시세 그래프가 표시됩니다.</p>
+            ) : (
+              <div>
+                <h3>실거래 가격(도매)</h3>
+                <ForecastChart detailCodeId={form.lowCodeValue} timeFrame={form.timeFrame} />
+              </div>
+            )}
+
+            <DividerLine />
+
+            <h2 style={{fontSize:'22px'}}>등락율</h2>
+
+            <PriceChangeTable />
           </div>
         </div>
-
-        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom:'10px' }}>
-          <label
-            htmlFor="selectedPeriod"
-            className="input-label"
-            style={{ marginRight: '10px', fontSize: '18px' }}
-          >
-            기간
-          </label>
-          <CustomRadio
-            value={form.timeFrame}
-            onChange={handlePeriodChange}
-            options={[
-              { value: 'week', label: '주간' },
-              { value: 'month', label: '월간' },
-              { value: 'year', label: '연간' },
-            ]}
-          />
-        </div>
-        <ReportDownloadButton form={form}></ReportDownloadButton>
       </div>
-
-      <div className="viewChart">
-        <h2 style={{ marginTop: '70px' }}>시세 추이</h2>
-
-        {form.lowCodeValue === '' ? (
-          <p>품목을 선택하면 시세 그래프가 표시됩니다.</p>
-        ) : (
-          <div>
-            <h3>실거래 가격(도매)</h3>
-            <ForecastChart detailCodeId={form.lowCodeValue} timeFrame={form.timeFrame} />
-          </div>
-        )}
-
-        <DividerLine />
-
-        <h2>등락율</h2>
-
-        <PriceChangeTable />
-        {/*<p className="trendy-price-caution" style={{ marginTop: '20px'}}>*/}
-        {/*  ※ 주말에는 최신 시세 데이터가 제공되지 않아, 가장 최근 평일(목요일 또는 금요일)의 가격을*/}
-        {/*  참고용으로 표시합니다.<br/>*/}
-        {/*  시세 추이는 어제와 그제의 가격을 비교하여 산출되며, 거래 내역이 있는 품목만 출력됩니다.*/}
-        {/*</p>*/}
-      </div>
-    </div>
   )
 }
 
