a56cc07e50e42e0aa2ca7cb5c608d7b11a563e29
seohay-1ae <mokash1ae@gmail.com>
2025-07-25 15:14:18 +0900

    [update] 챗봇 버튼 크기 ++ / 사이트 소개 주요기능 카드들 호버시 cursor

--- yk2hyeong/frontend/src/components/about/KeyFeatures.jsx
+++ yk2hyeong/frontend/src/components/about/KeyFeatures.jsx
@@ -1,31 +1,41 @@
-import React from "react";
-import {useNavigate} from "react-router-dom";
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-function KeyFeatures(){
-    const navigate = useNavigate();
+function KeyFeatures() {
+  const navigate = useNavigate()
 
-    return(
-        <div className="key-features">
-            <h1>주요기능</h1>
-            <div className="key-features-grid">
-                <div className="key-card" onClick={()=> navigate("/productlist")}>
-                    <i className="fa-solid fa-truck-fast"></i>
-                    <h2>즉시구매/예약구매</h2>
-                    <p>금일 시세로 즉시구매 또는 예약구매가 가능합니다.</p>
-                </div>
-                <div className="key-card" onClick={()=>navigate("/chart")}>
-                    <i className="fa-solid fa-chart-simple"></i>
-                    <h2>시세추이 차트</h2>
-                    <p>데이터 분석 차트를 보고 구매 결정에 참고할 수 있습니다.</p>
-                </div>
-                <div className="key-card">
-                    <i className="fa-solid fa-robot key-icon" />
-                    <h2>1:1 챗봇</h2>
-                    <p>AI 챗봇이 1:1 상담을 제공합니다.</p>
-                </div>
-            </div>
+  return (
+    <div className="key-features">
+      <h1>주요기능</h1>
+      <div className="key-features-grid">
+        <div
+          className="key-card"
+          style={{ cursor: 'pointer' }}
+          onClick={() => navigate('/productlist')}
+        >
+          <i className="fa-solid fa-truck-fast"></i>
+          <h2>즉시구매/예약구매</h2>
+          <p>금일 시세로 즉시구매 또는 예약구매가 가능합니다.</p>
         </div>
-    )
+        <div className="key-card" style={{ cursor: 'pointer' }} onClick={() => navigate('/chart')}>
+          <i className="fa-solid fa-chart-simple"></i>
+          <h2>시세추이 차트</h2>
+          <p>데이터 분석 차트를 보고 구매 결정에 참고할 수 있습니다.</p>
+        </div>
+        <div
+          className="key-card"
+          style={{ cursor: 'pointer' }}
+          onClick={() =>
+            window.open('http://192.168.0.139:5000/', '_blank', 'width=700,height=700')
+          }
+        >
+          <i className="fa-solid fa-robot key-icon" />
+          <h2>1:1 챗봇</h2>
+          <p>AI 챗봇이 1:1 상담을 제공합니다.</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default KeyFeatures;
+export default KeyFeatures
--- yk2hyeong/frontend/src/components/common/Footer.jsx
+++ yk2hyeong/frontend/src/components/common/Footer.jsx
@@ -1,50 +1,63 @@
-import React,{useEffect, useState} from "react";
-import "./footer.css";
-import { Link } from "react-router-dom";
-import CustomFloatButton from "./CustomFloatButton";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRobot } from '@fortawesome/free-solid-svg-icons';
-import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import './footer.css'
+import { Link } from 'react-router-dom'
+import CustomFloatButton from './CustomFloatButton'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRobot } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 export default function Footer() {
-    const [latestNotice, setLatestNotice] = useState(null);
-    const navigate = useNavigate();
-    useEffect(() => {
-        axios.get('/notice/latest')
-            .then(res => setLatestNotice(res.data))
-            .catch(err => console.error('공지사항 불러오기 실패:', err));
-    }, []);
-    return (
-        <footer className="footer-container">
-            <div className="footer-left">
-                <div className="notice-box" onClick={()=> {
-                    if (latestNotice){
-                        navigate(`/notice/${latestNotice.noticeId}`);
-                }
-                }} style={{cursor: 'pointer'}}>
-                    <h4>공지사항</h4>
-                    <p>{latestNotice?.noticeTitle || '공지사항이 없습니다.'}</p>
-                </div>
-                <div className="footer-info">
-                    [250704] 충청남도 천안시 동남구 대흥로 215 7층 <br />
-                    Copyright &copy; Yk2hyeong. All Rights Reserved.
-                </div>
-            </div>
-            <div className="footer-right">
-                <Link to="/">
-                    <img
-                        src={process.env.PUBLIC_URL + '/static/images/logo.png'}
-                        alt="로고"
-                        className="logo-img"
-                    />
-                </Link>
-            </div>
-            <CustomFloatButton
-                icon={<FontAwesomeIcon icon={faRobot} style={{ fontSize: '18px' }} />}
-                tooltip="AI 챗봇"
-                onClick={() => window.open("http://192.168.0.139:5000/", "_blank", "width=700,height=700")}
-            />
-        </footer>
-    );
+  const [latestNotice, setLatestNotice] = useState(null)
+  const navigate = useNavigate()
+  useEffect(() => {
+    axios
+      .get('/notice/latest')
+      .then((res) => setLatestNotice(res.data))
+      .catch((err) => console.error('공지사항 불러오기 실패:', err))
+  }, [])
+  return (
+    <footer className="footer-container">
+      <div className="footer-left">
+        <div
+          className="notice-box"
+          onClick={() => {
+            if (latestNotice) {
+              navigate(`/notice/${latestNotice.noticeId}`)
+            }
+          }}
+          style={{ cursor: 'pointer' }}
+        >
+          <h4>공지사항</h4>
+          <p>{latestNotice?.noticeTitle || '공지사항이 없습니다.'}</p>
+        </div>
+        <div className="footer-info">
+          [250704] 충청남도 천안시 동남구 대흥로 215 7층 <br />
+          Copyright &copy; Yk2hyeong. All Rights Reserved.
+        </div>
+      </div>
+      <div className="footer-right">
+        <Link to="/">
+          <img
+            src={process.env.PUBLIC_URL + '/static/images/logo.png'}
+            alt="로고"
+            className="logo-img"
+          />
+        </Link>
+      </div>
+      <CustomFloatButton
+        icon={<FontAwesomeIcon icon={faRobot} style={{ fontSize: '32px' }} />} // 아이콘 크기 키움
+        tooltip="AI 챗봇"
+        style={{
+          width: 64,
+          height: 64,
+          borderRadius: 32,
+          fontSize: 32,
+          right: 40,
+          bottom: 40,
+        }}
+        onClick={() => window.open('http://192.168.0.139:5000/', '_blank', 'width=700,height=700')}
+      />
+    </footer>
+  )
 }
